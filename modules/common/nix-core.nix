ba8c9859033ff1e9e5f985d477eed3773227b7dd
dsielert <456592+davidsielert@users.noreply.github.com>
2025-07-08 18:59:17 -0700

    --amend

--- modules/common/nix-core.nix
+++ modules/common/nix-core.nix
@@ -11,6 +11,15 @@
   nix = {
     settings = {
       experimental-features = "nix-command flakes";
+      trusted-users = [ "root" "dsielert" ];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      # key published on https://app.cachix.org/cache/nix-community
+      "nix-community.cachix.org-1:iH5YI1D3vds8ILsPzQXTrCG/tvY1pG+sGbGjfm6u5gI="
+    ];
     };
     optimise.automatic = true;
     package = pkgs.nix;
