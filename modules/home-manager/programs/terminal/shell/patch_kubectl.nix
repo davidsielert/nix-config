c675347a8c1e8373acd26e316eecbcfa3e91ace1
David Sielert <456592+davidsielert@users.noreply.github.com>
2025-07-14 00:27:55 +0200

    update

--- modules/home-manager/programs/terminal/shell/patch_kubectl.nix
+++ modules/home-manager/programs/terminal/shell/patch_kubectl.nix
@@ -1,17 +1,26 @@
-{lib, ...}: {
-  home.file.".config/fish/conf.d/plugin-kubectl.fish" = lib.mkForce {
-    # <- mkForce here!
-    text = ''
-      # patched blackjid/plugin-kubectl
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  target = "${config.xdg.configHome}/fish/conf.d/plugin-kubectl.fish";
+in {
+  # plugin stays in programs.fish.plugins
+  # …
 
-      set -q fisher_path; or set -l fisher_path $__fish_config_dir
-
-      if test -f $fisher_path/functions/__kubectl.init.fish
-          source $fisher_path/functions/__kubectl.init.fish
-          if functions -q __kubectl.init
-              __kubectl.init
-          end
-      end
-    '';
-  };
+  ## patch AFTER Home-Manager links the original file
+  home.activation.patchKubectl = lib.hm.dag.entryAfter ["linkGeneration"] ''
+        if [ -e "${target}" ]
+          # replace the broken 'source' line with a guarded version
+          sed -i -e '/source .*__kubectl.init.fish/,+2c\
+    set -q fisher_path; or set -l fisher_path $__fish_config_dir\n\
+    if test -f $fisher_path/functions/__kubectl.init.fish\n\
+        source $fisher_path/functions/__kubectl.init.fish\n\
+        if functions -q __kubectl.init\n\
+            __kubectl.init\n\
+        end\n\
+    end' "${target}"
+        end
+  '';
 }
