25731a987a0079142a059448c722859699a90eba
David Sielert <456592+davidsielert@users.noreply.github.com>
2025-07-12 19:02:05 -0700

    WLR renderer options

--- home/dsielert/nixosvm/default.nix
+++ home/dsielert/nixosvm/default.nix
@@ -6,7 +6,11 @@
 
   # Enable home-manager
   programs.home-manager.enable = true;
-
+  home.sessionVariables = {
+    XDG_SESSION_TYPE = "wayland";
+    WLR_RENDERER_ALLOW_SOFTWARE = "1";
+    NIXOS_OZONE_WL = "1";
+  };
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "25.05";
 }
