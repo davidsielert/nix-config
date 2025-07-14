08c34693f17fa0cc60d0e0de96219236e512e55e
David Sielert <456592+davidsielert@users.noreply.github.com>
2025-07-14 09:10:04 -0700

    fixing cdg alias

--- modules/home-manager/programs/terminal/shell/default.nix
+++ modules/home-manager/programs/terminal/shell/default.nix
@@ -120,7 +120,7 @@
       };
       cdg = {
         body = ''
-          set root (git rev-parse --show-toplevel ^/dev/null)
+          set root (git rev-parse --show-toplevel 2>/dev/null)
           if test -n "$root"
             cd $root
           else
